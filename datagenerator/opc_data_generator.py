04bc036ea7594997af4d804ac87e51a61639aa13
FJ Reig <fjreig@pavener.com>
2025-02-12 07:52:10 +0100

    cambios

--- datagenerator/opc_data_generator.py
+++ datagenerator/opc_data_generator.py
@@ -35,6 +35,8 @@ async def main():
     idx = await server.register_namespace(uri)
 
     (v_string_Inv1, i_string_Inv1) = await InversorSchema(server, idx, "Inversor1")
+    (tensiones_aarr, intensidades_aarr) = await AARRSchema(server, idx, "AARR1")
+    (radiaciones_emi1, temperaturas_emi1) = await EMISchema(server, idx, "EMI1")
   
     #_logger.info("Starting server!")
     async with server:
@@ -46,7 +48,18 @@ async def main():
                 await opc.write_value(new_val)
             for opc in i_string_Inv1:
                 new_val = round(random.uniform(is_min_val, is_max_val),2)
-                #_logger.info("Set value of %s to %.1f", opc, new_val)
+                await opc.write_value(new_val)
+            for opc in tensiones_aarr:
+                new_val = round(random.uniform(tensiones_min_val, tensiones_max_val),2)
+                await opc.write_value(new_val)
+            for opc in intensidades_aarr:
+                new_val = round(random.uniform(intensidades_min_val, intensidades_max_val),2)
+                await opc.write_value(new_val)
+            for opc in radiaciones_emi1:
+                new_val = round(random.uniform(rad_min_val, rad_max_val),2)
+                await opc.write_value(new_val)
+            for opc in temperaturas_emi1:
+                new_val = round(random.uniform(temp_min_val, temp_max_val),2)
                 await opc.write_value(new_val)
             
 if __name__ == "__main__":
