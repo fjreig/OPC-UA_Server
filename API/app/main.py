5f3cbcb0ead38da44bf917cc6c77754ce4d50ffa
FJ Reig <fjreig@pavener.com>
2025-02-12 13:55:52 +0100

    cambios

--- API/app/main.py
+++ API/app/main.py
@@ -8,6 +8,10 @@ from app.routes.Postgres import router as Postgres
 # Initialize the database
 init_db()
 
+description = """
+Pavener OPC-UA API. 🚀
+"""
+
 tags_metadata = [
     {
         "name": "Opc",
@@ -18,7 +22,26 @@ tags_metadata = [
     }
 ]
 
-app = FastAPI()
+app = FastAPI(
+    root_path="/api",
+    #docs_url=None, 
+    redoc_url=None,
+    title="API OPC-UA",
+    #lifespan=_lifespan,
+    description=description,
+    version="0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Pavener Servicios Energeticos SLU",
+        "url": "https://www.pavener.com/contacto/",
+        "email": "info@pavener.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    openapi_tags=tags_metadata
+)
 
 app.include_router(Opc, tags=["Opc"], prefix="/Opc")
 app.include_router(Postgres, tags=["Postgres"], prefix="/Postgres")
